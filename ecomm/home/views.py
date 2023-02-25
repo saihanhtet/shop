a85953efd1923db66321d017f01a23fe0a1161af
SaiHanHtet <hanhtetsan13@gmail.com>
2023-02-25 13:14:08 +0630

    update

--- ecomm/home/views.py
+++ ecomm/home/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    context = {
+        "products": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 23, 567, 7676, 5465767]
+    }
+    return render(request, 'home/index.html', context=context)
